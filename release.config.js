690666ac9c39bf39eca14cc15671009a9366a0f5
MurtajaKadiyani <murtaja.mainframe@gmail.com>
2022-06-05 20:51:23 +0530

    Upload codecov code coverage report

--- release.config.js
+++ release.config.js
@@ -1,19 +1,17 @@
 module.exports = {
   branches: "master",
   repositoryUrl: "https://github.com/MurtajaKadiyani/react-app",
-  plugins: 
-  [
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
-        "@semantic-release/github",
-        {
-            assets:
-            [
-                { path: "build.zip", label: "Build" },
-                { path: "coverage.zip", label: "Coverage" }
-            ]
-        }
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" }
+        ]
+      }
     ]
   ]
 };
