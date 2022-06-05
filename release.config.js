35f3dcefd9d4e293334967428a04c92279c22f33
MurtajaKadiyani <murtaja.mainframe@gmail.com>
2022-06-05 19:12:16 +0530

    feat: some feature

--- release.config.js
+++ release.config.js
@@ -1,10 +1,9 @@
-module.exports = 
-{
-    branches: "master",
-    repositoryUrl: "https://github.com/MurtajaKadiyani/react-app",
-    plugins: [
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator', 
-        '@semantic-release/github'
-    ]
-}
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/MurtajaKadiyani/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
