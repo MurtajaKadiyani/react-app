23c5e9fd33a96850f9c9f3fd732a360732e8cd0a
MurtajaKadiyani <murtaja.mainframe@gmail.com>
2022-06-16 11:07:42 +0530

    docs(dist): github Core Use

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -8866,13 +8866,22 @@ const github = __nccwpck_require__(5438);
 try
 {
   //throw(new Error("Some error message"));
+
+  core.debug('Debug message')
+  core.warning('Warning message')
+  core.error('Error message')
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toString());
-
-  console.log(JSON.stringify(github,null,'\t'));  
+  
+  core.startGroup('Logging github object')
+  console.log(JSON.stringify(github,null,'\t'));
+  core.endGroup();
+  core.exportVariable('HELLO','hello')  
 }
 catch(error)
 {
